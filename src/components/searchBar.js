27d41deeaedc2061a19893f05abb570d2b8cc955
tonyjhuang <huangtonyj91@gmail.com>
2018-12-31 16:39:36 -0500

    Add onFormSubmit() and onSearchInputChange()

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,13 +1,49 @@
 import React from 'react'
 
-class searchBar extends React.Component {
+class SearchBar extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: '',
+      location: 'San Francisco, CA'
+    }
+  }
+
+  onFormSubmit = event => {
+    event.preventDefault();
+    console.log(this.state, 'GET from Yelp');
+  };
+  
+
+  onSearchInputChange = e => {
+    this.setState({searchTerm: e.target.value});
+  }
+
+  onLocationInputChange = e => {
+    this.setState({location: e.target.value});
+  }
+
   render () {
     return (
-      <form>
-        <input></input>
-      </form>
-    )
+      <div>
+        <form onSubmit={this.onFormSubmit}>
+            {/* <label>Search</label> */}
+            <input 
+              type='text' 
+              onChange={this.onSearchInputChange}
+              value={this.state.searchTerm}
+              placeholder='Search'
+              />
+            {/* <input 
+              type='text' 
+              onChange={this.onLocationInputChange}
+              value={this.state.location}
+              /> */}
+        </form>
+      </div>
+    );
   }
 }
 
-export default searchBar;
+export default SearchBar;
